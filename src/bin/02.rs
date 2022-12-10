bec8a23788f88d286fc478f380d54174bd88729f
shangsuru <helm.henry99@gmail.com>
2022-12-10 11:07:16 +0900

    Introduce Round struct and use type conversion traits

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,9 +1,9 @@
-use std::process::exit;
+use std::str::FromStr;
 
 enum GameResult {
-    Win,
-    Draw,
     Loss,
+    Draw,
+    Win,
 }
 
 impl GameResult {
@@ -14,74 +14,145 @@ impl GameResult {
             GameResult::Loss => 0,
         }
     }
+
+    fn matching_choice(self, opponent: Choice) -> Choice {
+        match self {
+            GameResult::Loss => opponent.losing_choice(),
+            GameResult::Draw => opponent.drawing_choice(),
+            GameResult::Win => opponent.winning_choice(),
+        }
+    }
 }
 
-#[derive(PartialEq)]
+impl TryFrom<char> for GameResult {
+    type Error = String;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'X' => Ok(GameResult::Loss),
+            'Y' => Ok(GameResult::Draw),
+            'Z' => Ok(GameResult::Win),
+            _ => Err("Expected X, Y or Z to encode Loss, Draw or Win respectively".to_string()),
+        }
+    }
+}
+
+#[derive(PartialEq, Clone, Copy)]
 enum Choice {
     Rock,
     Paper,
-    Scissor,
+    Scissors,
 }
 
 impl Choice {
-    fn to_points(&self) -> u32 {
+    fn to_points(self) -> u32 {
         match self {
             Choice::Rock => 1,
             Choice::Paper => 2,
-            Choice::Scissor => 3,
+            Choice::Scissors => 3,
         }
     }
 
     fn against(self, opponent: Choice) -> GameResult {
-        if self == opponent {
-            return GameResult::Draw;
+        if self == opponent.losing_choice() {
+            return GameResult::Loss;
         }
-        if self == get_winning_choice(opponent) {
+        if self == opponent.winning_choice() {
             return GameResult::Win;
         }
-        GameResult::Loss
+
+        GameResult::Draw
+    }
+
+    fn winning_choice(self) -> Self {
+        match self {
+            Choice::Rock => Choice::Paper,
+            Choice::Paper => Choice::Scissors,
+            Choice::Scissors => Choice::Rock,
+        }
+    }
+
+    fn losing_choice(self) -> Self {
+        match self {
+            Choice::Rock => Choice::Scissors,
+            Choice::Paper => Choice::Rock,
+            Choice::Scissors => Choice::Paper,
+        }
+    }
+
+    fn drawing_choice(self) -> Self {
+        self
     }
 }
 
-fn get_winning_choice(opponent: Choice) -> Choice {
-    match opponent {
-        Choice::Rock => Choice::Paper,
-        Choice::Paper => Choice::Scissor,
-        Choice::Scissor => Choice::Rock,
+impl TryFrom<char> for Choice {
+    type Error = String;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'A' | 'X' => Ok(Choice::Rock),
+            'B' | 'Y' => Ok(Choice::Paper),
+            'C' | 'Z' => Ok(Choice::Scissors),
+            _ => Err(
+                "Expected A,B,C or X,Y,Z respectively to encode Rock, Paper, Scissors".to_string(),
+            ),
+        }
     }
 }
 
-fn get_losing_choice(opponent: Choice) -> Choice {
-    match opponent {
-        Choice::Rock => Choice::Scissor,
-        Choice::Paper => Choice::Rock,
-        Choice::Scissor => Choice::Paper,
+struct Round {
+    player: Choice,
+    opponent: Choice,
+}
+
+impl Round {
+    fn outcome(self) -> GameResult {
+        self.player.against(self.opponent)
+    }
+
+    fn score(self) -> u32 {
+        self.player.to_points() + self.outcome().to_points()
     }
 }
 
-fn to_choice(input: &str) -> Choice {
-    match input {
-        "A" | "X" => Choice::Rock,
-        "B" | "Y" => Choice::Paper,
-        "C" | "Z" => Choice::Scissor,
-        _ => exit(1),
+impl FromStr for Round {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chars = s.chars();
+        let (Some(opponent), Some(' '), Some(player), None) = (chars.next(), chars.next(), chars.next(), chars.next()) else {
+            return Err("Expected <opponent> <player>, got {s:?}".to_string());
+        };
+
+        Ok(Self {
+            player: player.try_into()?,
+            opponent: opponent.try_into()?,
+        })
+    }
+}
+
+struct Round2(Round);
+
+impl FromStr for Round2 {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chars = s.chars();
+        let (Some(opponent), Some(' '), Some(result), None) = (chars.next(), chars.next(), chars.next(), chars.next()) else {
+            return Err("Expected <opponent> <player>, got {s:?}".to_string());
+        };
+
+        let opponent = Choice::try_from(opponent)?;
+        let result = GameResult::try_from(result)?;
+        let player = result.matching_choice(opponent);
+        Ok(Self(Round { player, opponent }))
     }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut score: u32 = 0;
-
-    for line in input.split('\n') {
-        if line.is_empty() {
-            continue;
-        }
-
-        let mut choices = line.split_whitespace();
-        let opponent: Choice = to_choice(choices.next().unwrap());
-        let player: Choice = to_choice(choices.next().unwrap());
-
-        score += player.to_points();
-        score += player.against(opponent).to_points()
+    let mut score = 0;
+    for round in input.lines().map(Round::from_str) {
+        score += round.unwrap().score();
     }
 
     Some(score)
@@ -90,19 +161,8 @@ pub fn part_one(input: &str) -> Option<u32> {
 pub fn part_two(input: &str) -> Option<u32> {
     let mut score: u32 = 0;
 
-    for line in input.split('\n') {
-        if line.is_empty() {
-            continue;
-        }
-
-        let mut choices = line.split_whitespace();
-        let opponent: Choice = to_choice(choices.next().unwrap());
-        match choices.next().unwrap() {
-            "X" => score += GameResult::Loss.to_points() + get_losing_choice(opponent).to_points(),
-            "Y" => score += GameResult::Draw.to_points() + opponent.to_points(),
-            "Z" => score += GameResult::Win.to_points() + get_winning_choice(opponent).to_points(),
-            _ => exit(1),
-        }
+    for round in input.lines().map(Round2::from_str) {
+        score += round.unwrap().0.score();
     }
 
     Some(score)
