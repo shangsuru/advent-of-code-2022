de71cfe339b03ac4ed07a7c25fef5c2e0902c484
shangsuru <helm.henry99@gmail.com>
2022-12-10 13:12:30 +0900

    Introduce Item to only represent letters of the alphabet

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,108 +1,84 @@
+use item::Item;
+use itertools::Itertools;
 use std::collections::HashSet;
 
-struct Rucksack<'a> {
-    first_compartment: &'a str,
-    second_compartment: &'a str,
-}
+mod item {
 
-struct Group<'a> {
-    elf1: Rucksack<'a>,
-    elf2: Rucksack<'a>,
-    elf3: Rucksack<'a>,
-}
+    #[repr(transparent)]
+    #[derive(Clone, Copy, PartialEq, Eq, Hash)]
+    pub(crate) struct Item(u8);
 
-impl<'a> Rucksack<'a> {
-    fn new(input: &str) -> Rucksack {
-        let (first_compartment, second_compartment) = input.split_at(input.len() / 2);
-        Rucksack {
-            first_compartment,
-            second_compartment,
+    impl TryFrom<u8> for Item {
+        type Error = String;
+
+        fn try_from(value: u8) -> Result<Self, Self::Error> {
+            match value {
+                b'a'..=b'z' | b'A'..=b'Z' => Ok(Item(value)),
+                _ => Err("Expects only alphabetic characters".to_string()),
+            }
         }
     }
 
-    fn common_item(&self) -> char {
-        let mut set1 = HashSet::new();
-        for c in self.first_compartment.chars() {
-            set1.insert(c);
+    impl Item {
+        pub(crate) fn priority(self) -> u32 {
+            match self {
+                Item(b'a'..=b'z') => 1 + (self.0 - b'a') as u32,
+                Item(b'A'..=b'Z') => 27 + (self.0 - b'A') as u32,
+                _ => unreachable!(),
+            }
         }
-        let mut set2 = HashSet::new();
-        for c in self.second_compartment.chars() {
-            set2.insert(c);
-        }
-
-        *set1.intersection(&set2).into_iter().next().unwrap()
-    }
-
-    fn all_items(&self) -> HashSet<char> {
-        let mut set = HashSet::new();
-        for c in self.first_compartment.chars() {
-            set.insert(c);
-        }
-        for c in self.second_compartment.chars() {
-            set.insert(c);
-        }
-        set
-    }
-}
-
-impl<'a> Group<'a> {
-    fn new(input: [&str; 3]) -> Group {
-        Group {
-            elf1: Rucksack::new(input[0]),
-            elf2: Rucksack::new(input[1]),
-            elf3: Rucksack::new(input[2]),
-        }
-    }
-
-    fn badge(&self) -> char {
-        let intersection12: HashSet<char> = self
-            .elf1
-            .all_items()
-            .intersection(&self.elf2.all_items())
-            .cloned()
-            .collect();
-        let set3 = self.elf3.all_items();
-        let intersection = intersection12.intersection(&set3);
-        *intersection.into_iter().next().unwrap()
-    }
-}
-
-fn get_priority(item: char) -> u32 {
-    if item.is_lowercase() {
-        (item as u32 - 'a' as u32) + 1
-    } else if item.is_uppercase() {
-        (item as u32 - 'A' as u32) + 27
-    } else {
-        0
     }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(
-        input
-            .lines()
-            .filter(|line| !line.is_empty())
-            .map(|line| {
-                let item = Rucksack::new(line).common_item();
-                get_priority(item)
-            })
-            .sum(),
-    )
+    let sum: u32 = input
+        .lines()
+        .map(|line| {
+            let (first, second) = line.split_at(line.len() / 2);
+            let first_compartment = first
+                .bytes()
+                .map(Item::try_from)
+                .collect::<Result<HashSet<Item>, _>>()
+                .unwrap();
+            let second_compartment = second
+                .bytes()
+                .map(Item::try_from)
+                .collect::<Result<HashSet<Item>, _>>()
+                .unwrap();
+
+            let common_item = first_compartment
+                .intersection(&second_compartment)
+                .into_iter()
+                .next()
+                .unwrap();
+            common_item.priority()
+        })
+        .sum();
+
+    Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let mut sum: u32 = 0;
-    let mut it = input.lines();
+    let rucksacks = input.lines().map(|line| {
+        line.bytes()
+            .map(Item::try_from)
+            .collect::<Result<HashSet<_>, _>>()
+    });
 
-    loop {
-        let lines: Vec<&str> = it.by_ref().take(3).collect();
-        if lines.len() < 3 {
-            break Some(sum);
-        }
+    let sum = itertools::process_results(rucksacks, |rs| {
+        rs.tuples()
+            .map(|(a, b, c)| {
+                a.iter()
+                    .copied()
+                    .find(|i| b.contains(i) && c.contains(i))
+                    .map(|i| i.priority())
+                    .unwrap_or_default()
+            })
+            .sum::<u32>()
+    })
+    .unwrap();
 
-        let badge = Group::new(lines.try_into().unwrap()).badge();
-        sum += get_priority(badge);
-    }
+    Some(sum)
 }
 
 fn main() {
@@ -118,7 +94,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 3);
-        assert_eq!(part_one(&input), Some(12));
+        assert_eq!(part_one(&input), Some(38));
     }
 
     #[test]
