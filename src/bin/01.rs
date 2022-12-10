2619dcf6203330d2776c6c8447e01f407bbedb30
shangsuru <helm.henry99@gmail.com>
2022-12-10 11:05:57 +0900

    Refactor day 01

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,7 +3,7 @@ use std::cmp::max;
 pub fn part_one(input: &str) -> Option<u32> {
     let mut max_calories: u32 = 0;
     let mut elf_calories: u32 = 0;
-    for line in input.split('\n') {
+    for line in input.lines() {
         if line.is_empty() {
             max_calories = max(max_calories, elf_calories);
             elf_calories = 0;
@@ -18,7 +18,7 @@ pub fn part_two(input: &str) -> Option<u32> {
     let mut all_elves_calories: Vec<u32> = vec![];
     let mut current_elf_calories: Vec<u32> = vec![];
 
-    for line in input.split('\n') {
+    for line in input.lines() {
         if line.is_empty() {
             all_elves_calories.push(current_elf_calories.iter().sum());
             current_elf_calories = vec![];
@@ -27,7 +27,7 @@ pub fn part_two(input: &str) -> Option<u32> {
         }
     }
 
-    all_elves_calories.sort_by(|a, b| b.cmp(a));
+    all_elves_calories.sort_by_key(|&v| std::cmp::Reverse(v));
     Some(all_elves_calories.iter().take(3).sum())
 }
 
